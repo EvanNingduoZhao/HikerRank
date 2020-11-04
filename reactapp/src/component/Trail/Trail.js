f973f81b25036797cb590bfdba135a021a84bb41
Ningduo Zhao <ningduoz@andrew.cmu.edu>
2020-11-04 00:54:35 -0500

    completed trail page's header and pic section

--- reactapp/src/component/Trail/Trail.js
+++ reactapp/src/component/Trail/Trail.js
@@ -1,11 +1,38 @@
 import React, {Component} from "react";
+import '../../App.css';
+import './Trail.css';
+import trailPagePic from '../../pictures/TrailPagePic.png';
+import sampleMap from '../../pictures/sample-map.png'
+import Nav from '../Nav'
+import Search from '../Search'
+import SignUpButton from '../Signup/SignUpButton'
+import LoginButton from '../Login/LoginButton'
+
 
 class Trail extends Component {
     render() {
         return (
-            <div>
+            <div className = 'container'>
+                <div className='header-container'>
+                    <div><h3 className='title'>HIKERRANK</h3></div> 
+                    <Nav />
+                    <Search />
+                    <LoginButton />
+                    <SignUpButton />
+                </div>
+
+                <div className = 'main-content-container'>
+                    <div className='pic-slogan'>
+                    <img className='bg-image' src={trailPagePic} width='100%'/>
+                        <h1 className='slogan'>Riverview Park Adventure</h1>
+                        <h3 className='sub-slogan'>This is a scenic starting and ending at the Observatory, taking you past several key places and arround the park</h3>
+                    </div>
+
+
+                </div>
 
             </div>
+
         );
     }
 }
