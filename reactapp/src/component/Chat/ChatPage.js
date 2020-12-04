36c6abd00409cc06df25b5c293a94dfa1bd25e61
Ningyang Peng <jerrypeng02@gmail.com>
2020-12-04 17:01:01 -0500

    added login check when directly going to the chat page

--- reactapp/src/component/Chat/ChatPage.js
+++ reactapp/src/component/Chat/ChatPage.js
@@ -3,11 +3,9 @@ import WebSocketInstance from "./WebSocket";
 import './Chat.css';
 import Chat from "./Chat";
 import Sidepanel from "./Sidepanel";
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 import Nav from "../Nav";
 import Footer from "../Footer";
-import LoginButton from "../Login/LoginButton";
-import SignUpButton from "../Signup/SignUpButton";
 import DropDownMenu from "../DropDownMenu";
 
 class ChatPage extends Component {
@@ -20,48 +18,33 @@ class ChatPage extends Component {
         }
     }
 
-    componentDidMount() {
-        WebSocketInstance.connect(this.state.chat_id);
-    }
-
     render() {
-        const renderLoginButton = () => {
-            if(this.state.login_status!=='true'){
-                return (
-                    <LoginButton />
-                    )
+        const renderChatPage = () => {
+            if(this.state.login_status !== 'true') {
+                return (<Redirect to='/login' />);
             } else {
-                return (<p className="welcome-msg">Hello, {this.state.username}! :)</p >)
-            }
-        }
-
-        const renderSignupButton = () => {
-            if(this.state.login_status!=='true'){
+                WebSocketInstance.connect(this.state.chat_id);
                 return (
-                    <SignUpButton />
-                    )
-            } else {
-                return (<DropDownMenu />)
-            }
-        }
-
-        return (
-            <div>
-                <div className='header-container'>
-                    <div><h3 className='title'><Link to='/'>HIKERRANK</Link></h3></div>
-                    <Nav />
-                    {renderLoginButton()}
-                    {renderSignupButton()}
-                </div>
-                <div className="chat-page">
-                    <div id="frame">
-                        <Sidepanel />
-                        <Chat chat_id = {this.state.chat_id} />
+                    <div>
+                        <div className='header-container'>
+                            <div><h3 className='title'><Link to='/'>HIKERRANK</Link></h3></div>
+                            <Nav />
+                            <p className="welcome-msg">Hello, {this.state.username}! :)</p >
+                            <DropDownMenu />
+                        </div>
+                        <div className="chat-page">
+                            <div id="frame">
+                                <Sidepanel />
+                                <Chat chat_id = {this.state.chat_id} />
+                            </div>
+                        </div>
+                        <Footer />
                     </div>
-                </div>
-                <Footer />
-            </div>
-        );
+                );
+            }
+        }
+
+        return (renderChatPage());
     }
 }
 
