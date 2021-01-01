1d22750adc2b7384ded6460bf0eb3ba5d6c34785
EvanNingduoZhao <46782811+EvanNingduoZhao@users.noreply.github.com>
2020-12-31 19:02:54 -0500

    hide keys

--- webapps/settings.py
+++ webapps/settings.py
@@ -18,8 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$#1v#m-_!#b8e-os%*9p!9fb8j95332&$gtwxlajx4_e^*g=va'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
